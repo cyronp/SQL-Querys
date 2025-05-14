c9168fd7cb7867345a7245ab6e84caada86d53a9
cyronp <vitorhenriquedasilveira@gmail.com>
2025-05-13 22:01:59 -0300

    SQL Start point

---
+++ RegistrarNotas.sql
@@ -0,0 +1,96 @@
+GO
+-- CADASTRAR NOTA
+CREATE PROCEDURE CadastraNotas
+	(
+		@MATRICULA INT,
+		@CURSO CHAR(3),
+		@MATERIA CHAR(3),
+		@PERLETIVO CHAR(4),
+		@NOTA FLOAT,
+		@FALTA INT,
+		@BIMESTRE INT
+	)
+	AS
+BEGIN
+
+		IF @BIMESTRE = 1
+		    BEGIN
+
+                UPDATE MATRICULA
+                SET N1 = @NOTA,
+                    F1 = @FALTA,
+                    TOTALPONTOS = @NOTA,
+                    TOTALFALTAS = @FALTA,
+                    MEDIA = @NOTA
+                WHERE MATRICULA = @MATRICULA
+                    AND CURSO = @CURSO
+                    AND MATERIA = @MATERIA
+                    AND PERLETIVO = @PERLETIVO;
+		    END
+
+        ELSE 
+        
+        IF @BIMESTRE = 2
+            BEGIN
+
+                UPDATE MATRICULA
+                SET N2 = @NOTA,
+                    F2 = @FALTA,
+                    TOTALPONTOS = @NOTA + N1,
+                    TOTALFALTAS = @FALTA + F1,
+                    MEDIA = (@NOTA + N1) / 2
+                WHERE MATRICULA = @MATRICULA
+                    AND CURSO = @CURSO
+                    AND MATERIA = @MATERIA
+                    AND PERLETIVO = @PERLETIVO;
+            END
+
+        ELSE 
+        
+        IF @BIMESTRE = 3
+            BEGIN
+
+                UPDATE MATRICULA
+                SET N3 = @NOTA,
+                    F3 = @FALTA,
+                    TOTALPONTOS = @NOTA + N1 + N2,
+                    TOTALFALTAS = @FALTA + F1 + F2,
+                    MEDIA = (@NOTA + N1 + N2) / 3
+                WHERE MATRICULA = @MATRICULA
+                    AND CURSO = @CURSO
+                    AND MATERIA = @MATERIA
+                    AND PERLETIVO = @PERLETIVO;
+            END
+
+        ELSE 
+        
+        IF @BIMESTRE = 4
+            BEGIN
+
+                DECLARE @RESULTADO VARCHAR(50),
+                        @FREQUENCIA FLOAT,
+                        @MEDIAFINAL FLOAT,
+                        @CARGAHORA INT 
+                
+                SET @CARGAHORA = (
+                    SELECT CARGAHORARIA FROM MATERIAS 
+                    WHERE       SIGLA = @MATERIA
+                            AND CURSO = @CURSO)
+
+                UPDATE MATRICULA
+                SET N4 = @NOTA,
+                    F4 = @FALTA,
+                    TOTALPONTOS = @NOTA + N1 + N2 + N3,
+                    TOTALFALTAS = @FALTA + F1 + F2 + F3,
+                    MEDIA = (@NOTA + N1 + N2 + N3) / 4
+                WHERE MATRICULA = @MATRICULA
+                    AND CURSO = @CURSO
+                    AND MATERIA = @MATERIA
+                    AND PERLETIVO = @PERLETIVO;
+
+
+            END
+            
+
+		SELECT * FROM MATRICULA	WHERE MATRICULA = @MATRICULA
+END
