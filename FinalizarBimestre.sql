9386b14ea77f8ba5a338ddaae116101663cc164e
cyronp <vitorhenriquedasilveira@gmail.com>
2025-05-20 15:07:53 -0300

    Update FinalizarBimestre.sql

--- FinalizarBimestre.sql
+++ FinalizarBimestre.sql
@@ -11,14 +11,30 @@ BEGIN
     SET 
         TOTALFALTAS = (F1 + F2 + F3 + F4),
         MEDIAFINAL = (N1 + N2 + N3 + N4) / 4,
-        PERCFREQ = 100 - (((F1 + F2 + F3 + F4) * 144) / 100),
+        PERCFREQ = 100 - (((F1 + F2 + F3 + F4) * 100.0) / (
+            SELECT CARGAHORARIA 
+            FROM MATERIAS 
+            WHERE MATERIAS.SIGLA = MATRICULA.MATERIA 
+            AND MATERIAS.CURSO = MATRICULA.CURSO
+        )),
         RESULTADO = CASE
-                       WHEN (N1 + N2 + N3 + N4) / 4 >= 7 AND PERCFREQ > 75 THEN 'APROVADO'
-                       WHEN (N1 + N2 + N3 + N4) / 4 < 7 AND PERCFREQ > 75 THEN 'EXAME'
-                       ELSE 'REPROVADO'
+                    WHEN (N1 + N2 + N3 + N4) / 4 >= 7 
+                         AND (100 - (((F1 + F2 + F3 + F4) * 100.0) / (
+                            SELECT CARGAHORARIA 
+                            FROM MATERIAS 
+                            WHERE MATERIAS.SIGLA = MATRICULA.MATERIA 
+                            AND MATERIAS.CURSO = MATRICULA.CURSO
+                        ))) > 75 THEN 'APROVADO'
+                    WHEN (N1 + N2 + N3 + N4) / 4 < 7 
+                         AND (100 - (((F1 + F2 + F3 + F4) * 100.0) / (
+                            SELECT CARGAHORARIA 
+                            FROM MATERIAS 
+                            WHERE MATERIAS.SIGLA = MATRICULA.MATERIA 
+                                AND MATERIAS.CURSO = MATRICULA.CURSO
+                        ))) > 75 THEN 'EXAME'
+                    ELSE 'REPROVADO'
                     END
-    WHERE MATRICULA = @MATRICULA
-      AND PERLETIVO = @PERLETIVO
-      AND MATERIA = @MATERIA
-      AND CURSO = @CURSO;
+    WHERE PERLETIVO = (SELECT MAX(PERLETIVO) FROM MATRICULA);
+END;
+GO
 END;
